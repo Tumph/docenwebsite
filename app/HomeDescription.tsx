98e681525015f7d631c1bca38123406ea8449df1
Jonathan Lyashko <lyashko.jonathan@gmail.com>
2024-04-21 16:15:01 -0300

    Add Jon's rebuild of the production website

--- app/HomeDescription.tsx
+++ app/HomeDescription.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
 
-import FlowChart from '@/public/PareFlowChart.webp';
 import CroppedFlowChart from '@/public/PareFlowChart.png'
 
 function HomeDescription() {
