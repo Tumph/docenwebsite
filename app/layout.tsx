bdb037b8f1192d0e2e5a4590aa630c612c96542a
Tumph <aryansmail@gmail.com>
2024-11-25 16:38:09 -0500

    added preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,28 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Docen",
   description: "Supercharge your L&D with AI.",
+  openGraph: {
+    title: 'Docen',
+    description: 'Supercharge your L&D with AI.',
+    url: 'https://joindocen.com',
+    siteName: 'Docen',
+    images: [
+      {
+        url: '/preview.jpg',
+        width: 1024,
+        height: 683,
+        alt: 'Docen Preview Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Docen',
+    description: 'Supercharge your L&D with AI.',
+    images: ['/preview.jpg'],
+  },
   icons: {
     icon: [
       { url: '/favicon.ico'},
